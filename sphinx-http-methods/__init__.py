331c080e392e7af6ceb8e3aa8407b9e2997a8280
OguzErtanAyanlar <oguzertanayanlar@gmail.com>
2019-12-24 23:55:26 +0200

    e

--- sphinx-http-methods/__init__.py
+++ sphinx-http-methods/__init__.py
@@ -1,14 +1,14 @@
 from docutils import nodes
 from docutils.parsers.rst import Directive
 
-class HelloWorld(Directive):
+class HTTPPost(Directive):
     def run(self):
         paragraph_node = nodes.paragraph(text='POST')
         paragraph_node['classes'].append('api-method-post api-method-text')
         return [paragraph_node]
 
 def setup(app):
-    app.add_directive("helloworld", HelloWorld)
+    app.add_directive("httppost", HTTPPost)
 
     return {
         'version': '1.0.0',
