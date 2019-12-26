67e5d1e0ee037bbbd1d195b342080e7504c46318
OguzErtanAyanlarW <ertan.ayanlar@marketspringpad.com>
2019-12-26 19:06:08 +0200

    d

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'sphinx-http-methods',
-    version = '1.4.2',
+    version = '1.4.3',
     author = 'OguzErtanAyanlar',
     author_email = 'oguzertanayanlar@gmail.com',
     packages = ['sphinx-http-methods'],
--- sphinx-http-methods/__init__.py
+++ sphinx-http-methods/__init__.py
@@ -22,6 +22,39 @@ class HTTPMethod(Directive):
 
         return [paragraph_node]
 
+def copy_assets(app, exception):
+    """ Copy asset files to the output """
+    if 'getLogger' in dir(logging):
+        log = logging.getLogger(__name__).info  # pylint: disable=no-member
+        warn = logging.getLogger(__name__).warning  # pylint: disable=no-member
+    else:
+        log = app.info
+        warn = app.warning
+    builders = get_compatible_builders(app)
+    if exception:
+        return
+    if app.builder.name not in builders:
+        if not app.config['sphinx_http-methods_nowarn']:
+            warn(
+                'Not copying tabs assets! Not compatible with %s builder' %
+                app.builder.name)
+        return
+
+    log('Copying tabs assets')
+
+    installdir = os.path.join(app.builder.outdir, '_static', 'sphinx-http-methods')
+
+    for path in FILES:
+        source = resource_filename('sphinx-http-methods', path)
+        dest = os.path.join(installdir, path)
+
+        destdir = os.path.dirname(dest)
+        if not os.path.exists(destdir):
+            os.makedirs(destdir)
+
+        copyfile(source, dest)
+
+
 def setup(app):
     app.add_directive("httpmethod", HTTPMethod)
 
@@ -32,8 +65,10 @@ def setup(app):
             else:
                 app.add_stylesheet(path)
 
+    app.connect('build-finished', copy_assets)
+
     return {
-        'version': '1.4.2',
+        'version': '1.4.3',
         'parallel_read_safe': True,
         'parallel_write_safe': True,
     }
