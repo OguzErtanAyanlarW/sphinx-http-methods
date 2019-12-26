afc5b2d2502fec21f7d16d17fc5357c01738150f
OguzErtanAyanlarW <ertan.ayanlar@marketspringpad.com>
2019-12-26 18:25:53 +0200

    d

--- sphinx-http-methods/__init__.py
+++ sphinx-http-methods/__init__.py
@@ -1,18 +1,28 @@
 from docutils import nodes
 from docutils.parsers.rst import Directive
 
-class HTTPMethod(nodes.Admonition, nodes.Element):
+class httpmethod(nodes.Admonition, nodes.Element):
     pass
+
+def visit_httpmethod_node(self, node):
+    self.visit_admonition(node)
+
+def depart_httpmethod_node(self, node):
+    self.depart_admonition(node)
 
 class HelloWorld(Directive):
     has_content = True
     required_arguments = 1
 
     def run(self):
-        http_method = HTTPMethod(text=self.arguments[0])
+        http_method = httpmethod(text=self.arguments[0])
         return [http_method]
 
 def setup(app):
+    app.add_node(httpmethod,
+                 html=(visit_httpmethod_node, depart_httpmethod_node),
+                 latex=(visit_httpmethod_node, depart_httpmethod_node),
+                 text=(visit_httpmethod_node, depart_httpmethod_node))
     app.add_directive("helloworld", HelloWorld)
 
     return {
