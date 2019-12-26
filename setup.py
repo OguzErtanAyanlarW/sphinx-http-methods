e8c52718255a6ec7ebeb5d93fafa9289868bdcb2
OguzErtanAyanlarW <ertan.ayanlar@marketspringpad.com>
2019-12-26 19:14:08 +0200

    d

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'sphinx-http-methods',
-    version = '1.4.7',
+    version = '1.4.8',
     author = 'OguzErtanAyanlar',
     author_email = 'oguzertanayanlar@gmail.com',
     packages = ['sphinx-http-methods'],
--- sphinx-http-methods/__init__.py
+++ sphinx-http-methods/__init__.py
@@ -82,7 +82,7 @@ def setup(app):
     app.connect('build-finished', copy_assets)
 
     return {
-        'version': '1.4.7',
+        'version': '1.4.8',
         'parallel_read_safe': True,
         'parallel_write_safe': True,
     }
